7fe8c8b754c0b1e08ca460ba8c73da8d4ffb4514
Matheus Marinho Rodrigues <rodriguesm.2005@alunos.utfpr.edu.br>
2024-09-02 14:34:19 -0300

    ajustado erro

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -3,10 +3,14 @@ const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 exports.register = async (req, res) => {
-    const { username, password, role } = req.body;
+    const { username, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
     try {
-        const user = await User.create({ username, password: hashedPassword, role });
+        const user = await User.create({
+            username,
+            password: hashedPassword,
+            role: "user",
+        });
         res.status(201).json(user);
     } catch (error) {
         res.status(400).json({ error: 'User could not be created' });
